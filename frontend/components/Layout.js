cf013508381959bfce6bc152e40393f33d39f45b
Sddilora <sumeyyedilaradogan@gmail.com>
2023-04-26 20:14:53 +0300

    <a> error handled

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -3,35 +3,33 @@ import Link from 'next/link';
 import React from 'react';
 
 export default function Layout({ title, children }) {
-  return (
-    <>
-      <Head>
-        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
-        <meta name="description" content="Ecommerce Website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  return <>
+    <Head>
+      <title>{title ? title + ' - SD' : 'SD'}</title>
+      <meta name="description" content="Ecommerce Website" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <div className="flex min-h-screen flex-col justify-between ">
-        <header>
-          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
-            <Link href="/">
-              <a className="text-lg font-bold">amazona</a>
+    <div className="flex min-h-screen flex-col justify-between ">
+      <header>
+        <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+          <Link href="/" className="text-lg font-bold">
+            SDD
+          </Link>
+          <div>
+            <Link href="/cart" className="p-2">
+              Cart
             </Link>
-            <div>
-              <Link href="/cart">
-                <a className="p-2">Cart</a>
-              </Link>
-              <Link href="/login">
-                <a className="p-2">Login</a>
-              </Link>
-            </div>
-          </nav>
-        </header>
-        <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex h-10 justify-center items-center shadow-inner">
-          <p>Copyright © 2022 Amazona</p>
-        </footer>
-      </div>
-    </>
-  );
+            <Link href="/login" className="p-2">
+              Login
+            </Link>
+          </div>
+        </nav>
+      </header>
+      <main className="container m-auto mt-4 px-4">{children}</main>
+      <footer className="flex h-10 justify-center items-center shadow-inner">
+        <p>Copyright © 2023 SD</p>
+      </footer>
+    </div>
+  </>;
 }
--- frontend/components/Productitem.js
+++ frontend/components/Productitem.js
@@ -6,19 +6,19 @@ export default function ProductItem({ product }) {
   return (
     <div className="card">
       <Link href={`/product/${product.slug}`}>
-        <a>
-          <img
-            src={product.image}
-            alt={product.name}
-            className="rounded shadow"
-          />
-        </a>
+
+        <img
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow"
+        />
+
       </Link>
       <div className="flex flex-col items-center justify-center p-5">
         <Link href={`/product/${product.slug}`}>
-          <a>
-            <h2 className="text-lg">{product.name}</h2>
-          </a>
+
+          <h2 className="text-lg">{product.name}</h2>
+
         </Link>
         <p className="mb-2">{product.brand}</p>
         <p>${product.price}</p>
